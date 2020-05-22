ae07e7a7fcdcef4b45cc70413c1fd4447f2407d7
DmitriiBaranovskii <dmbt.baranovsky@gmail.com>
2020-05-22 15:00:20 +0300

    msg

--- 6_exception1.py
+++ 6_exception1.py
@@ -9,12 +9,23 @@
   и завершала работу при помощи оператора break
     
 """
+dialog = {'Как дела?': 'Хорошо', 'Что делаешь?': 'Программирую', 'На каком языке?': 'Python'}
 
 def ask_user():
-    """
-    Замените pass на ваш код
-    """
-    pass
-    
+# Не совсем понял где нужно раместить try внутри while True или снаружи
+    try:
+        while True:
+            message = input('Задайте вопрос: ')
+            for item in dialog:
+                if message == item:
+                    print(dialog.get(message))
+                    break
+                elif message != 'Пока':
+                    print('Увы, я вас не понимаю')
+                    break
+            if message == 'Пока':
+                break
+    except (KeyboardInterrupt):
+        print('\nПока!')
 if __name__ == "__main__":
     ask_user()
