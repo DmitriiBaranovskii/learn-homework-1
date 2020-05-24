4451d26964cfafb06e40c7007e357cb3539e5253
DmitriiBaranovskii <dmbt.baranovsky@gmail.com>
2020-05-24 19:38:36 +0300

    fix

--- 6_exception1.py
+++ 6_exception1.py
@@ -9,23 +9,19 @@
   и завершала работу при помощи оператора break
     
 """
-dialog = {'Как дела?': 'Хорошо', 'Что делаешь?': 'Программирую', 'На каком языке?': 'Python'}
 
 def ask_user():
 # Не совсем понял где нужно раместить try внутри while True или снаружи
     try:
-        while True:
+        dialog = {'Как дела?': 'Хорошо', 'Что делаешь?': 'Программирую', 'На каком языке?': 'Python'}
+        message = ''
+        while message != 'Пока':
             message = input('Задайте вопрос: ')
-            for item in dialog:
-                if message == item:
-                    print(dialog.get(message))
-                    break
-                elif message != 'Пока':
-                    print('Увы, я вас не понимаю')
-                    break
-            if message == 'Пока':
-                break
-    except (KeyboardInterrupt):
+            if message in dialog:
+                print(dialog.get(message))
+            elif message != 'Пока':
+                print('Увы, я вас не понимаю')
+    except KeyboardInterrupt:
         print('\nПока!')
 if __name__ == "__main__":
     ask_user()
